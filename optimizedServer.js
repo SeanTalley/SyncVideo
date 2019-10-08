7fd642321b8348f82ff962e500efe7955c746caf
SeanTalley <55466559+SeanTalley@users.noreply.github.com>
2019-10-08 14:59:18 -0500

    Add files via upload

---
+++ optimizedServer.js
@@ -0,0 +1,51 @@
+var express = require("express");
+var app = express();
+var server = require('http').createServer(app);
+var io = require('socket.io')(server, {pingInterval: 5000 });
+
+app.use(express.static('www'));
+
+var master = "";
+var masterLat = 0;
+var playing = false;
+var masterPos = 0;
+io.on('connection', function(socket) {
+  if(master == "") {
+    master = socket.id;
+    socket.on("latency", function(lat) {
+      masterLat = lat;
+    });
+    socket.on("pos", function(pos) {
+      socket.broadcast.emit("updatePos", pos + masterLat);
+      masterPos = pos + masterLat;
+    });
+    socket.on("play",function() {
+      socket.broadcast.emit("play");
+      playing = true;
+    });
+    socket.on("pause",function() {
+      socket.broadcast.emit("pause");
+      playing = false;
+    });
+    socket.on("disconnect",function() {
+      io.emit("masterdisconnect");
+    });
+    console.log("Master is " + socket.id);
+    socket.emit("master");
+  }
+  else {
+    socket.on("disconnect",function() {
+      console.log("Clients: " + Object.keys(io.sockets.connected).length);
+      io.emit("clients",Object.keys(io.sockets.connected).length);      
+    });
+    socket.emit("updatePos", masterPos + masterLat);
+    socket.emit("notmaster");
+    console.log("NotMaster is " + socket.id);
+    if(playing)
+      socket.emit("play");
+  }
+  console.log("Clients: " + Object.keys(io.sockets.connected).length);
+  io.emit("clients",Object.keys(io.sockets.connected).length);
+});
+console.log("Listening on port 8080");
+server.listen(8080);
